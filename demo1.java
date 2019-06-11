b8de811f54b5bb95225af412c1be107d10f85a62
srikumar <srikumar.venkatraman@gmail.com>
2019-06-11 08:46:27 +0530

    SEcond COmmit with add

--- demo1.java
+++ demo1.java
@@ -1,2 +1,4 @@
-total 0
+D:wq!
+
+idskksnfdsnkftotal 0
 -rw-r--r-- 1 root root 0 Jun 11 08:32 demo1.java
---
+++ demo2.java
@@ -0,0 +1,3 @@
+total 4
+-rw-r--r-- 1 root root 77 Jun 11 08:41 demo1.java
+-rw-r--r-- 1 root root  0 Jun 11 08:45 demo2.java
