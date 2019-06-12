d0493c13bdac6d809a67cf2dff282c8bbd09b511
srikumar <srikumar.venkatraman@gmail.com>
2019-06-12 07:23:07 +0530

    cOMMIT TO add demo2. and changes

--- demo1.java
+++ demo1.java
@@ -1,4 +1,4 @@
-D:wq!
-
+d
+testing the changes
 idskksnfdsnkftotal 0
 -rw-r--r-- 1 root root 0 Jun 11 08:32 demo1.java
--- demo2.java
+++
@@ -1,3 +0,0 @@
-total 4
--rw-r--r-- 1 root root 77 Jun 11 08:41 demo1.java
--rw-r--r-- 1 root root  0 Jun 11 08:45 demo2.java
